7ceeb3d0350a3043fecae45703d901fbb03e885c
liumin_mbp <317590898@qq.com>
2018-10-13 16:28:10 +0800

    Initial Commit

---
+++ GenericsInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenericsInSwift
+//
+//  Created by Bill liu on 2018/10/13.
+//  Copyright © 2018 Mzying. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
